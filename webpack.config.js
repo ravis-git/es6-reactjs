0c57ab2bf66e25b83e3ca281600533051d757ae9
Ravi Palakodeti <ravi.palakodeti@ge.com>
2017-07-05 07:41:34 -0500

    webpack config changed

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack')
 var path = require('path')
 
 module.exports = {
-    entry: './src/App.jsx',
+    entry: './app/App.jsx',
     output: {
         path: __dirname,
         filename: 'bundle.js'
